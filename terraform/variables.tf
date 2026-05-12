bf561a681451101ac793d1df85d51abb523eee9c
ManickaMeenakshi1306 <230701173@rajalakshmi.edu.in>
2026-05-13 02:19:09 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,19 @@
-variable "environment" {
-  default = "production"
+variable "supabase_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "supabase_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "gemini_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_storage_connection_string" {
+  type      = string
+  sensitive = true
 }
